219516dadb114cdb677dba9d98d71fe70066c26a
tediyo <thedronberhanu05@gmail.com>
2024-08-03 08:27:39 +0300

    Again

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
+    <div>
       <h1>Home Page</h1>
       <p>Welcome to the Home Page!</p>
       
@@ -14,6 +15,35 @@ export default function Home() {
           </li>
         </ul>
       </nav>
+    </div>
+
+    {/* ///////////////////////// */}
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Home Page!</p>
+      
+      <nav>
+        <ul>
+          <li>
+            <Link href="/page1">Go to Page 1</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    {/* //////////////////////////////////////////// */}
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Home Page!</p>
+      
+      <nav>
+        <ul>
+          <li>
+            <Link href="/page1">Go to Page 1</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
     </div>
   );
 }
