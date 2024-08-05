71965c7b525e5f4258a6d3ca3235cbeda2ec0756
tediyo <thedronberhanu05@gmail.com>
2024-08-05 08:33:43 +0300

    Third horizontal card added

--- src/app/components/MainContent.js
+++ src/app/components/MainContent.js
@@ -21,13 +21,33 @@ const MainContent = () => {
       >
         <Row gutter={[16, 16]}>
           <Col span={12}>
-            <Card title="Card 1" bordered={true}>
+            <Card 
+              title="Card 1" 
+              bordered={true}
+              style={{ backgroundColor: '#D9D9D9', minHeight: 300 }}
+            >
               Content for the first vertical card.
             </Card>
           </Col>
           <Col span={12}>
-            <Card title="Card 2" bordered={true}>
+            <Card 
+              title="Card 2" 
+              bordered={true}
+              style={{ backgroundColor: '#D9D9D9', minHeight: 300 }}
+            >
               Content for the second vertical card.
+            </Card>
+          </Col>
+        </Row>
+        
+        <Row gutter={[16, 16]} style={{ marginTop: 16 }}>
+          <Col span={24}>
+            <Card 
+              title="Card 3" 
+              bordered={true}
+              style={{ backgroundColor: '#D9D9D9', minHeight: 150 }}
+            >
+              Content for the horizontal card below.
             </Card>
           </Col>
         </Row>
